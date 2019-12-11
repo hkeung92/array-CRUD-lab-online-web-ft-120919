f216cca02b20ae5c73782c3c7f567c3315a726d4
Ho Yin Keung <hykeung92@gmail.com>
2019-12-11 01:46:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  []
+  create_an_empty_array = []
 end
 
 def create_an_array
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Wayne", "Keung", "Yurim", "Maeng"].push["Dolly"]
+  .push["Dolly"]
 end
 
 def add_element_to_start_of_array(array, element)
