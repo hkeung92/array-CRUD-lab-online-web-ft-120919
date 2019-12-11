767c9c7eb977929fa68aa6126d65e06efa2111da
Ho Yin Keung <hykeung92@gmail.com>
2019-12-11 01:46:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  .push["Dolly"]
+  create_an_array.push["Dolly"]
 end
 
 def add_element_to_start_of_array(array, element)
